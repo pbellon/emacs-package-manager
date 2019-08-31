06c0b727ef90c987438834efaa92c4af8b114d16
Pierre BELLON <bellon.pierre@gmail.com>
2019-08-31 17:41:45 +0200

    Makes function names consistants

--- manager.el
+++ manager.el
@@ -1,67 +1,67 @@
 ;;; manager.el --- pseudo dependency manager based on use-package  -*- lexical-binding: t -*-
+;; Author: Pierre BELLON <bellon.pierre@gmail.com>
 ;;; Commentary:
 
 ;;; The idea is to get a simple way to add & manage dependencies like following:
 ;; (setq manager/dependencies-config-dir "~/.emacs.d/packages")
-;; (manager/setup) and it will load every .el file listed under ~/.emac.d/packages/
+;; (manager/load-dependencies) and it will load every .el file listed under ~/.emac.d/packages/
 ;; 
 ;; Another functionnality would be to add with command similar as package-install
-;; with =M-x manager/add RET <dependency name>= that will add it to the
+;; with =MANAGER/x manager/add RET <dependency name>= that will add it to the
 ;; dependency file located at manager/dependency-config-file.
 (defcustom manager/dependencies-config-dir nil
   "The folder to use for dependencies configuration"
   :type 'string
   :group 'manager)
 
-(defun m-fname (name)
+(defcustom manager/dependency-template "(use-package %s\n  :ensure t)"
+  :type 'string
+  :group 'manager)
+
+(defun manager/fname (name)
   "Return the filname of the dependency"
   (format "%s/%s.el" manager/dependencies-config-dir name))
 
-(defun m-reload-dep (name)
+(defun manager/load-dependency (name)
   "Reload a single dependency, creates a <name>.el with a (user-package ...) template"
   (message "Loading %s depency" name) 
-  (load-file (m-fname name)))
+  (load-file (manager/fname name)))
 
-(defun m-open (name)
-  (find-file (m-fname name)))
+(defun manager/open (name)
+  (find-file (manager/fname name)))
 
-(defun m-create-new (name)
+(defun manager/create-new (name)
   "Creates a new dependency"
-  (unless (file-exists-p (m-fname name))
+  (unless (file-exists-p (manager/fname name))
     (write-region
-     (format "(use-package %s\n  :ensure t)" name)
+     (format manager/dependency-template name)
      nil
-     (m-fname name))
+     (manager/fname name))
     (message "Dependency already exists, doing nothing")))
 
-(defun m-list-dependencies ()
+(defun manager/list-dependencies ()
   (message "Going to list %s folder" manager/dependencies-config-dir)
   (let (dependencies)
     (dolist (el (directory-files manager/dependencies-config-dir nil "[\w-]*.el$") dependencies)
       (message "found %s" el)
       (setq dependencies (cons (file-name-sans-extension el) dependencies)))))
 
-(defun m-reload-deps (deps)
+(defun manager/load-dependencies-list (deps)
   (dolist (el deps)
-    (m-reload-dep el)))
+    (manager/load-dependency el)))
 
-(defun manager/setup ()
-  "Trigger dependency initialization, will create files if it does not exist"
-  (interactive)
-  (m-reload-deps (m-list-dependencies)))
-
-(defun manager/reload-dependencies ()
+(defun manager/load-dependencies ()
     "Load all defined dependencies"
     (interactive)
-    (message "[manager] - Reloading dependencies")
-    (m-reload-deps))
+    (message "Reloading dependencies")
+    (manager/load-dependencies-list (m-list-dependencies)))
 
 (defun manager/add ()
   "Create a new dependency file in the dependencies config dir, see =manager/dependencies-config-dir="
   (interactive)
-  (let ((name (read-string "[manager] - Dependency name?\n> ")))
-    (m-create-new name)
-    (m-open name)))
+  (let ((name (read-string "Dependency name?\n> ")))
+    (manager/create-new name)
+    (manager/open name)))
 
 (provide 'manager)
 ;;; manager.el ends here
